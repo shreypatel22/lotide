fb54a11b7c7f97cfa7d57b11bda3eb93c6f3a03e
Shrey Patel <shreypatel11@hotmail.com>
2022-06-03 00:41:34 -0400

    Added export to eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -22,11 +22,13 @@ const eqArrays = function(arrayOne, arrayTwo) {
 };
 
 // Test Case
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays([1, 1, 3], [1, 1, 3,]), true);
-assertEqual(eqArrays([], []), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+// assertEqual(eqArrays([1, 1, 3], [1, 1, 3,]), true);
+// assertEqual(eqArrays([], []), true);
 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1, 2]), false);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1, 2]), false);
+
+module.exports = eqArrays;
