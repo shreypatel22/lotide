dae57c96b37656b46966b7c322b8d1ee9d9a58ea
Shrey Patel <shreypatel11@hotmail.com>
2022-06-06 18:03:15 -0400

    Refactored tailTest to use Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,23 +1,20 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe('#tail', () => {
+  it("Returns [2, 'Three', 4] for ['One', 2, 'Three', 4]", () => {
+    const test1 = tail(['One', 2, 'Three', 4]);
+    assert.deepEqual(test1, [2, 'Three', 4]);
+  })
 
-// Test Cases
-const test1 = tail(['One', 2, 'Three', 4]);
-assertEqual(test1.length, 3);
-assertEqual(test1[0], 2);
-assertEqual(test1[1], 'Three');
-assertEqual(test1[2], '4');
+  it("Orginal array should not be changed.", () => {
+    const test2 = ['Yo Yo', 'Lighthouse', 'Labs'];
+    tail(test2);
+    assert.deepEqual(test2, ['Yo Yo', 'Lighthouse', 'Labs']);
+  });
+});
 
-//  Check the original array
-const test2 = ['Yo Yo', 'Lighthouse', 'Labs'];
-tail(test2);
-console.log("Testing to see if original array was changed.");
-assertEqual(test2.length, 3);
 
-// More Test Cases
-console.log("More Test Cases");
-const test3 = [1];
-assertEqual(tail(test3).length, 0);
-const test4 = [];
-assertEqual(tail(test4).length, 0);
+
+
+
