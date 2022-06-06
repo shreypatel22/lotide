a7f9b505850590dd08b14e5e41b860ac959e4343
Shrey Patel <shreypatel11@hotmail.com>
2022-06-06 16:11:48 -0400

    Refactored Head

--- head.js
+++ head.js
@@ -1,19 +1,5 @@
-// Function Declarations
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🔴🔴🔴Assertion failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const head = function(array) {
   return array[0];
 };
 
-// Test Cases
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(['1',2,3]), 1);
-assertEqual(head([99]), 99);
-assertEqual(head([]), undefined);
+module.exports = head;
