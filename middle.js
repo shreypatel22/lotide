d31766dae864ac14bd5b725cde0a39626168b6da
Shrey Patel <shreypatel11@hotmail.com>
2022-06-06 17:16:34 -0400

    Refactored middle and added middleTest

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-// Compare Arrays
-const eqArrays = function(arrayOne, arrayTwo) {
-  if(arrayOne.length !== arrayTwo.length){
-    return false;
-  }
-
-  for (let i = 0; i < arrayOne.length; i++){
-    if (arrayOne[i] !== arrayTwo[i]){
-      return false;
-    }
-  }
-  return true;
-};
-
-// Out to console whether the two arrays are the same or not
-const assertArraysEqual = function(array1, array2) {
-  if(eqArrays(array1, array2)){
-    console.log(`Passed -> [${array1}] === [${array2}]`);
-  } else {
-    console.log(`Failed -> [${array1}] !== [${array2}]`);
-  }
-};
-
 // find middle elements of a given array
 const middle = function(array) {
   let length = array.length;
@@ -37,9 +14,7 @@ const middle = function(array) {
   return middleArray;
 };
 
-// Test Cases
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+module.exports = middle;
+
+
+
